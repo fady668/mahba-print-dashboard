a23c09ac206bd6d12dde22ceb11a58fcf261340d
fady668 <fadygeorge668@gmail.com>
2024-06-25 00:46:29 +0300

    fix some issues

--- frontend/src/Componants/AdditionalRow.jsx
+++ frontend/src/Componants/AdditionalRow.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const AdditionalRow = (props) => {
-    const additional = props.additional;
-    const done = additional.done ? "تم الدفع" : "لم يتم الدفع";
-    return (
-        <>
-            <td></td>
-            <td>{additional.additional_type}</td>
-            <td>{additional.count}</td>
-            <td>{additional.salary_of_one}</td>
-            <td>{additional.total}</td>
-            <td>{done}</td>
-        </>
-    );
+  const additional = props.additional;
+  const done = additional.done ? "تم الدفع" : "لم يتم الدفع";
+  return (
+    <>
+      <td></td>
+      <td>{additional.additional_type}</td>
+      <td>{additional.count}</td>
+      <td>{parseFloat(additional.salary_of_one)}</td>
+      <td>{parseFloat(additional.total)}</td>
+      <td>{done}</td>
+    </>
+  );
 };
 
 export default AdditionalRow;
