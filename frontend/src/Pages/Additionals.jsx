4c74272730c3d01332e8bf67c9ed70220e8c5767
fady668 <fadygeorge668@gmail.com>
2024-07-19 22:31:56 +0300

    Fix Some Issues

--- frontend/src/Pages/Additionals.jsx
+++ frontend/src/Pages/Additionals.jsx
@@ -6,147 +6,152 @@ import AdditionalRow from "../Componants/AdditionalRow";
 import swal from "sweetalert";
 
 const Additionals = () => {
-  const clientId = useParams().id;
-  const [client, setClient] = useState({});
-  const [additionals, setAdditionals] = useState([]);
-  const [isPending, setIsPending] = useState(false);
-  const [showNewAdditional, setShowNewAdditional] = useState(false);
-  const [showUpdateAdditional, setShowUpdateAdditional] = useState(false);
-  const [updatedAdditional, setUpdatedAdditional] = useState(false);
-  const [totalAdditionalsMoney, setTotalAdditioansMoney] = useState(
-    parseFloat(0)
-  );
+    const clientId = useParams().id;
+    const [client, setClient] = useState({});
+    const [additionals, setAdditionals] = useState([]);
+    const [isPending, setIsPending] = useState(false);
+    const [showNewAdditional, setShowNewAdditional] = useState(false);
+    const [showUpdateAdditional, setShowUpdateAdditional] = useState(false);
+    const [updatedAdditional, setUpdatedAdditional] = useState(false);
+    const [totalAdditionalsMoney, setTotalAdditioansMoney] = useState(
+        parseFloat(0)
+    );
 
-  useEffect(() => {
-    getClient();
-    getAdditionals();
-  }, []);
+    useEffect(() => {
+        getClient();
+        getAdditionals();
+    }, []);
 
-  const getClient = async () => {
-    const res = await api.get(`api/clients/${clientId}`);
-    setClient(res.data[0]);
-  };
-  const getAdditionals = async () => {
-    setIsPending(true);
-    const res = await api.get(`api/additionals/byclientid/${clientId}`);
-    const data = await res.data;
-    setIsPending(false);
-    setAdditionals(data);
-    if (data.length != 0) {
-      let total = parseFloat(0);
-      for (let x = 0; x < data.length; x++) {
-        total += parseFloat(data[x].total);
-      }
-      setTotalAdditioansMoney(total);
-    }
-  };
+    const getClient = async () => {
+        const res = await api.get(`api/clients/${clientId}`);
+        setClient(res.data[0]);
+    };
+    const getAdditionals = async () => {
+        setIsPending(true);
+        const res = await api.get(`api/additionals/byclientid/${clientId}`);
+        const data = await res.data;
+        setIsPending(false);
+        setAdditionals(data);
+        if (data.length != 0) {
+            let total = parseFloat(0);
+            for (let x = 0; x < data.length; x++) {
+                total += parseFloat(data[x].remaining_cash);
+            }
+            setTotalAdditioansMoney(total);
+        }
+    };
 
-  const updateAdditional = (updatedAdd) => {
-    setShowUpdateAdditional(true);
-    setUpdatedAdditional(updatedAdd);
-  };
+    const updateAdditional = (updatedAdd) => {
+        setShowUpdateAdditional(true);
+        setUpdatedAdditional(updatedAdd);
+    };
 
-  const deleteMess = (deletedAdd) => {
-    swal({
-      title: "هل انت متأكد من مسح الاضافة",
-      icon: "warning",
-      buttons: true,
-    }).then((willDelete) => {
-      if (willDelete) {
-        api
-          .delete(`/api/additionals/${deletedAdd.id}`)
-          .catch((e) => console.log(e.response.data));
-        window.location.reload();
-      } else {
-        swal("لم يتم مسح الاضافة");
-      }
-    });
-  };
+    const deleteMess = (deletedAdd) => {
+        swal({
+            title: "هل انت متأكد من مسح الاضافة",
+            icon: "warning",
+            buttons: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                api.delete(`/api/additionals/${deletedAdd.id}`).catch((e) =>
+                    console.log(e.response.data)
+                );
+                window.location.reload();
+            } else {
+                swal("لم يتم مسح الاضافة");
+            }
+        });
+    };
 
-  return (
-    <div className="additionals-page">
-      <h1 className="page-title">الاضافات الخارجيه</h1>
-      <div className="page-content">
-        <button onClick={() => setShowNewAdditional(true)} className="add-btn">
-          أضافه جديدة
-        </button>
-        {showNewAdditional && (
-          <>
-            <span className="over-layout"></span>
-            <div className="invoise-form focused">
-              <span onClick={() => setShowNewAdditional(false)}>
-                <i className="fa-solid fa-xmark"></i>
-              </span>
-              <AdditionalForm
-                route={`/api/additionals/`}
-                method="add"
-                client={clientId}
-              />
+    return (
+        <div className="additionals-page">
+            <h1 className="page-title">الاضافات الخارجيه</h1>
+            <div className="page-content">
+                <button
+                    onClick={() => setShowNewAdditional(true)}
+                    className="add-btn">
+                    أضافه جديدة
+                </button>
+                {showNewAdditional && (
+                    <>
+                        <span className="over-layout"></span>
+                        <div className="invoise-form focused">
+                            <span onClick={() => setShowNewAdditional(false)}>
+                                <i className="fa-solid fa-xmark"></i>
+                            </span>
+                            <AdditionalForm
+                                route={`/api/additionals/`}
+                                method="add"
+                                client={clientId}
+                            />
+                        </div>
+                    </>
+                )}
+                {showUpdateAdditional && (
+                    <>
+                        <span className="over-layout"></span>
+                        <div className="invoise-form focused">
+                            <span
+                                onClick={() => setShowUpdateAdditional(false)}>
+                                <i className="fa-solid fa-xmark"></i>
+                            </span>
+                            <AdditionalForm
+                                route={`/api/additionals/${updatedAdditional.id}`}
+                                method="edit"
+                                add={updatedAdditional}
+                                client={clientId}
+                            />
+                        </div>
+                    </>
+                )}
+                <span className="heighlight-heading">{client.name}</span>
+                <div className="total-cash not-border">
+                    <span>اجمالي حساب الاضافات</span>
+                    <span className="heighlight-text">
+                        {totalAdditionalsMoney}
+                    </span>
+                </div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <td>الترتيب</td>
+                            <td>الصنف</td>
+                            <td>العدد</td>
+                            <td>سعر الوحده</td>
+                            <td>الاجمالي</td>
+                            <td>الدفع</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {additionals.length != 0 ? (
+                            additionals.map((add) => (
+                                <tr key={add.id} className="sorted-row">
+                                    <AdditionalRow additional={add} />
+                                    <td>
+                                        <i
+                                            onClick={() =>
+                                                updateAdditional(add)
+                                            }
+                                            className="fa-solid fa-pen-to-square"></i>
+                                        <i
+                                            onClick={() => deleteMess(add)}
+                                            className="fa-solid fa-trash"></i>
+                                    </td>
+                                </tr>
+                            ))
+                        ) : isPending ? (
+                            <span className="loading table-loading"></span>
+                        ) : (
+                            <tr key={0} className="notfound">
+                                <td>لا يوجد أضافات قديمه</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
-          </>
-        )}
-        {showUpdateAdditional && (
-          <>
-            <span className="over-layout"></span>
-            <div className="invoise-form focused">
-              <span onClick={() => setShowUpdateAdditional(false)}>
-                <i className="fa-solid fa-xmark"></i>
-              </span>
-              <AdditionalForm
-                route={`/api/additionals/${updatedAdditional.id}`}
-                method="edit"
-                add={updatedAdditional}
-                client={clientId}
-              />
-            </div>
-          </>
-        )}
-        <span className="heighlight-heading">{client.name}</span>
-        <div className="total-cash not-border">
-          <span>اجمالي حساب الاضافات</span>
-          <span className="heighlight-text">{totalAdditionalsMoney}</span>
         </div>
-        <table className="table">
-          <thead>
-            <tr>
-              <td>الترتيب</td>
-              <td>الصنف</td>
-              <td>العدد</td>
-              <td>سعر الوحده</td>
-              <td>الاجمالي</td>
-              <td>الدفع</td>
-              <td></td>
-            </tr>
-          </thead>
-          <tbody>
-            {additionals.length != 0 ? (
-              additionals.map((add) => (
-                <tr key={add.id} className="sorted-row">
-                  <AdditionalRow additional={add} />
-                  <td>
-                    <i
-                      onClick={() => updateAdditional(add)}
-                      className="fa-solid fa-pen-to-square"
-                    ></i>
-                    <i
-                      onClick={() => deleteMess(add)}
-                      className="fa-solid fa-trash"
-                    ></i>
-                  </td>
-                </tr>
-              ))
-            ) : isPending ? (
-              <span className="loading table-loading"></span>
-            ) : (
-              <tr key={0} className="notfound">
-                <td>لا يوجد أضافات قديمه</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Additionals;
